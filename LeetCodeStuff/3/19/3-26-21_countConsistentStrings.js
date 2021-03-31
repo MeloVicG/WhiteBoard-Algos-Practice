15db3261a548352f96d4a36bb736e846335b1f54
MeloVicG <Melovicg41@gmail.com>
2021-03-31 09:29:59 -0700

    morning practice

--- LeetCodeStuff/3/19/3-26-21_countConsistentStrings.js
+++ LeetCodeStuff/3/19/3-26-21_countConsistentStrings.js
@@ -2,6 +2,13 @@
 //  * @param {string} allowed
 //  * @param {string[]} words
 //  * @return {number}
+
+// You are given a string allowed consisting of distinct characters 
+// and an array of strings words. A string is consistent if all characters 
+// in the string appear in the string allowed.
+// Return the number of consistent strings in the array words.
+
+
 //  */
 // var countConsistentStrings = function (allowed, words) {
 
