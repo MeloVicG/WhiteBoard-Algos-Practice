2b2f45fec3147971f0ef89d49f06a01ffe77848a
MeloVicG <Melovicg41@gmail.com>
2021-05-05 00:14:13 -0700

    2sums

---
+++ HackerRankStuff/LeftRotation.js
@@ -0,0 +1,16 @@
+
+
+
+
+leftRotate = (a, d) =>{
+    //make forloop to iterate through array
+    for(i = 0; i <= d; i++){}
+    //
+    //create a temp for each loop swap number with
+    //
+}
+
+arr = [1,2,3,4,5]
+num = 4
+
+leftRotate(arr, num)
